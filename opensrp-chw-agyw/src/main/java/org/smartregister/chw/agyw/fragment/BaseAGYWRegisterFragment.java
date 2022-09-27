c37248f635bedc562c6aad9796e9521552bea9c4
Billy <billypatrick767@gmail.com>
2022-09-27 13:13:15 +0300

    cleanup: fragment

--- opensrp-chw-agyw/src/main/java/org/smartregister/chw/agyw/fragment/BaseAGYWRegisterFragment.java
+++ opensrp-chw-agyw/src/main/java/org/smartregister/chw/agyw/fragment/BaseAGYWRegisterFragment.java
@@ -1,17 +1,16 @@
 package org.smartregister.chw.agyw.fragment;
 
 import android.widget.ImageView;
-import android.widget.TextView;
 
+import org.smartregister.agyw.R;
 import org.smartregister.chw.agyw.activity.BaseAGYWProfileActivity;
 import org.smartregister.chw.agyw.contract.AGYWRegisterFragmentContract;
 import org.smartregister.chw.agyw.model.BaseAGYWRegisterFragmentModel;
 import org.smartregister.chw.agyw.presenter.BaseAGYWRegisterFragmentPresenter;
+import org.smartregister.chw.agyw.provider.AGYWRegisterProvider;
 import org.smartregister.commonregistry.CommonPersonObjectClient;
 import org.smartregister.configurableviews.model.View;
 import org.smartregister.cursoradapter.RecyclerViewPaginatedAdapter;
-import org.smartregister.agyw.R;
-import org.smartregister.chw.agyw.provider.AGYWRegisterProvider;
 import org.smartregister.view.customcontrols.CustomFontTextView;
 import org.smartregister.view.customcontrols.FontVariant;
 import org.smartregister.view.fragment.BaseRegisterFragment;
@@ -19,9 +18,13 @@ import org.smartregister.view.fragment.BaseRegisterFragment;
 import java.util.HashMap;
 import java.util.Set;
 
+import androidx.appcompat.widget.Toolbar;
+
 public class BaseAGYWRegisterFragment extends BaseRegisterFragment implements AGYWRegisterFragmentContract.View {
     public static final String CLICK_VIEW_NORMAL = "click_view_normal";
     public static final String FOLLOW_UP_VISIT = "follow_up_visit";
+    protected Toolbar toolbar;
+    protected CustomFontTextView titleView;
 
     @Override
     public void initializeAdapter(Set<View> visibleColumns) {
@@ -34,6 +37,11 @@ public class BaseAGYWRegisterFragment extends BaseRegisterFragment implements AG
     @Override
     public void setupViews(android.view.View view) {
         super.setupViews(view);
+
+        toolbar = view.findViewById(R.id.register_toolbar);
+        toolbar.setContentInsetsAbsolute(0, 0);
+        toolbar.setContentInsetsRelative(0, 0);
+        toolbar.setContentInsetStartWithNavigation(0);
 
         // Update top left icon
         qrCodeScanImageView = view.findViewById(org.smartregister.R.id.scanQrCode);
@@ -50,24 +58,29 @@ public class BaseAGYWRegisterFragment extends BaseRegisterFragment implements AG
             getSearchView().setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_action_search, 0, 0, 0);
         }
 
-        // Update sort filter
-        TextView filterView = view.findViewById(org.smartregister.R.id.filter_text_view);
-        if (filterView != null) {
-            filterView.setText(getString(R.string.sort));
-        }
 
-        // Update title name
+        //hide views
         ImageView logo = view.findViewById(org.smartregister.R.id.opensrp_logo_image_view);
-        if (logo != null) {
-            logo.setVisibility(android.view.View.GONE);
-        }
+        logo.setVisibility(android.view.View.GONE);
+        android.view.View topLeftLayout = view.findViewById(R.id.top_left_layout);
+        topLeftLayout.setVisibility(android.view.View.GONE);
+        android.view.View topRightLayout = view.findViewById(R.id.top_right_layout);
+        topRightLayout.setVisibility(android.view.View.VISIBLE);
+        android.view.View sortFilterBarLayout = view.findViewById(R.id.register_sort_filter_bar_layout);
+        sortFilterBarLayout.setVisibility(android.view.View.GONE);
+        android.view.View filterSortLayout = view.findViewById(R.id.filter_sort_layout);
+        filterSortLayout.setVisibility(android.view.View.GONE);
 
-        CustomFontTextView titleView = view.findViewById(R.id.txt_title_label);
+        titleView = view.findViewById(R.id.txt_title_label);
         if (titleView != null) {
             titleView.setVisibility(android.view.View.VISIBLE);
-            titleView.setText(getString(R.string.agyw));
+            titleView.setText(getTitleString());
             titleView.setFontVariant(FontVariant.REGULAR);
         }
+    }
+
+    protected int getTitleString() {
+        return R.string.menu_AGYW;
     }
 
     @Override
