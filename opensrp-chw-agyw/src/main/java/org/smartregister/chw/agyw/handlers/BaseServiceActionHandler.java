a2931a824ccb0d43a4ae1f03cab4ba245987e418
Billy <billypatrick767@gmail.com>
2022-09-27 12:55:04 +0300

    fix: action handler to load the form on click

--- opensrp-chw-agyw/src/main/java/org/smartregister/chw/agyw/handlers/BaseServiceActionHandler.java
+++ opensrp-chw-agyw/src/main/java/org/smartregister/chw/agyw/handlers/BaseServiceActionHandler.java
@@ -1,10 +1,14 @@
 package org.smartregister.chw.agyw.handlers;
 
+import android.app.Activity;
 import android.content.Context;
 import android.view.View;
 import android.widget.Toast;
 
 import org.smartregister.agyw.R;
+import org.smartregister.chw.agyw.activity.BaseServicesFormActivity;
+import org.smartregister.chw.agyw.dao.AGYWDao;
+import org.smartregister.chw.agyw.domain.MemberObject;
 import org.smartregister.chw.agyw.domain.ServiceCard;
 import org.smartregister.chw.agyw.util.Constants;
 
@@ -17,13 +21,14 @@ public class BaseServiceActionHandler implements View.OnClickListener {
         ServiceCard serviceCard = (ServiceCard) view.getTag();
         String baseEntityID = (String) view.getTag(R.id.BASE_ENTITY_ID);
         if (i == R.id.card_layout) {
-            startVisitActivity(view.getContext(), serviceCard, baseEntityID);
+            MemberObject member = AGYWDao.getMember(baseEntityID);
+            startVisitActivity(view.getContext(), serviceCard, member);
         }
     }
 
-    protected void startVisitActivity(Context context, ServiceCard serviceCard, String baseEntityId) {
+    protected void startVisitActivity(Context context, ServiceCard serviceCard, MemberObject memberObject) {
         if (serviceCard.getServiceId().equals(Constants.SERVICES.AGYW_BEHAVIORAL_SERVICES)) {
-            //something happened
+            BaseServicesFormActivity.startMe((Activity) context, serviceCard.getFormName(), memberObject.getBaseEntityId(), memberObject.getAge());
             return;
         }
         Toast.makeText(context, serviceCard.getServiceName() + "Loading activity... ", Toast.LENGTH_SHORT).show();
